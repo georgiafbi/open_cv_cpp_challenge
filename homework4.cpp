6eefea36d67403727c155769442dd036d1e91146
Destany Brown <Georgiafbi@gmail.com>
2021-09-22 16:34:55 -0400

    added SIFT

--- homework4.cpp
+++ homework4.cpp
@@ -5,17 +5,25 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/features2d.hpp"
-// #include "opencv2/xfeatures2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
 using namespace cv;
-// using namespace cv::xfeatures2d;
+using namespace cv::xfeatures2d;
 using std::cout;
 using std::endl;
 
+
+
 int main(int argc, char* argv[]){
-
-    // Mat img = imread("C:/Users/Silho/OneDrive/Desktop/adv_robot_programming/open_cv_cpp_challenge/open_cv_cpp_challenge/BookScene.png");
+    
+    Mat img = imread("C:/Users/Silho/OneDrive/Desktop/adv_robot_programming/open_cv_cpp_challenge/open_cv_cpp_challenge/BookScene.png",IMREAD_GRAYSCALE);
+    
+      if ( img.empty())
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        return -1;
+    }
     // imshow("Book Scene",img);
-
+    // waitKey(20);
     VideoCapture cap("C:/Users/Silho/OneDrive/Desktop/adv_robot_programming/open_cv_cpp_challenge/open_cv_cpp_challenge/BookScene.MOV");
 
     // cap is the object of class video capture that tries to capture Bumpy.mp4
@@ -25,32 +33,32 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
-    // double fps = cap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
-    // The function get is used to derive a property from the element.
-    // Example:
-    // CV_CAP_PROP_POS_MSEC :  Current Video capture timestamp.
-    // CV_CAP_PROP_POS_FRAMES : Index of the next frame.
-
-    //namedWindow("A_good_name",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-    // first argument: name of the window.
-    // second argument: flag- types: 
-    // WINDOW_NORMAL : The user can resize the window.
-    // WINDOW_AUTOSIZE : The window size is automatically adjusted to fitvthe displayed image() ), and you cannot change the window size manually.
-    // WINDOW_OPENGL : The window will be created with OpenGL support.
+    
     namedWindow("Book Scene", 1);
+
+     //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+    int minHessian = 400;
+    Ptr<SIFT> detector = SIFT::create();
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+    Mat descriptors_object, descriptors_scene;
+
+    detector->detectAndCompute( img, noArray(), keypoints_object, descriptors_object );
     while(1)
     {
+        Mat grayFrame;
         Mat frame;
         // Mat object is a basic image container. frame is an object of Mat.
         cap >> frame; //get a new frame from video
+        //convert color frame to gray frame
+        cvtColor(frame, grayFrame,COLOR_BGR2GRAY);
         if (!cap.read(frame)) // if not success, break loop
         // read() decodes and captures the next frame.
         {
             cout<<"\n Cannot read the video file. \n";
             break;
         }
-
-        imshow("Book Scene", frame);
+        // detector->detectAndCompute( grayFrame, noArray(), keypoints_scene, descriptors_scene );
+        imshow("Book Scene", grayFrame);
         // first argument: name of the window.
         // second argument: image to be shown(Mat object).
 
